20417729e77b801478a30edd5a732bc420b41001
hpaulini <helenpaulini@gmail.com>
2018-07-17 15:30:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,14 +6,4 @@ class App < Sinatra::Base
 
 end
 
-get '/name' do
-    "My name is Helen, Eliza, Rachel"
-  end
-  
-  gets '/hometown' do
-    "My hometown is Boston"
-  end 
-  
-  gets '/favorite-song' do
-    "My favorite song is Baby"
-  end
+

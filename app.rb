3cf66e650939fce6a286fdc9d033521015fb989e
hpaulini <helenpaulini@gmail.com>
2018-07-17 15:30:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get '/name' do
-    "Helen, Eliza, Rachel"
+    "My name is Helen, Eliza, Rachel"
   end
   
+  gets '/hometown' do
+    "My hometown is Boston"
+  end 
   
-  
+  gets '/favorite-song' do
+    "My favorite song is Baby"
 end
